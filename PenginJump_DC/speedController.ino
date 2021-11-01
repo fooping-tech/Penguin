30db19d7c6c4241ab15999bed1bdfc63dfbfd99d
bicicic <finedeepblue@gmai.com>
2021-11-01 22:54:11 +0900

    設定されているスピードを取得する関数を追加

--- PenginJump_DC/speedController.ino
+++ PenginJump_DC/speedController.ino
@@ -3,6 +3,14 @@
 
 Servo esc;
 
+// 現在設定されているスピード(-100% ~ 100%)
+int speedController_Speed = 0;
+
+// 現在設定されているスピード(-100% ~ 100%)を返す
+int speedController_GetSpeed() {
+  return speedController_Speed;
+}
+
 // ESC初期化処理
 void speedController_Setup() {
   esc.attach(speedController_PIN);
@@ -12,6 +20,7 @@ void speedController_Setup() {
 
 // モータ停止処理
 void speedController_Stop() {
+  speedController_Speed = 0;
   esc.writeMicroseconds(speedController_NEUTRAL);
 }
 
@@ -24,6 +33,7 @@ void speedController_Output(int percent) {
   if (100 < percent) {
     percent = 100;
   }
+  speedController_Speed = percent;
   const float k = ((float)speedController_MAX - (float)speedController_MIN)/200.;
   int pulseTime = (int)(k * ((float)percent + 100.)) + speedController_MIN;
   esc.writeMicroseconds(pulseTime);
